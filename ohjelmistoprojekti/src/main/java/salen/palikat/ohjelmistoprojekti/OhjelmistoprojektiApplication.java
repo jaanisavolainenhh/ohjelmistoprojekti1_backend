b8b3040e8f0ed8b909f79babde3c68e6b07c6e96
Jaani Savolainen <bgh273@myy.haaga-helia.fi>
2020-04-07 14:28:21 +0300

    Aloitus

---
+++ ohjelmistoprojekti/src/main/java/salen/palikat/ohjelmistoprojekti/OhjelmistoprojektiApplication.java
@@ -0,0 +1,13 @@
+package salen.palikat.ohjelmistoprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjelmistoprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhjelmistoprojektiApplication.class, args);
+	}
+
+}
---
+++ ohjelmistoprojekti/src/test/java/salen/palikat/ohjelmistoprojekti/OhjelmistoprojektiApplicationTests.java
@@ -0,0 +1,13 @@
+package salen.palikat.ohjelmistoprojekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhjelmistoprojektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
