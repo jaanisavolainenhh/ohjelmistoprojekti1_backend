392738466d9ddf02946ddbe3a5401142211e2940
jaani <bgh273@myy.haaga-helia.fi>
2020-04-12 13:41:49 +0300

    Lisätty  variablet kysymykselle, nyt  pitäs voida postaa vastaus että mihin kysymykseen

--- src/main/java/salen/palikat/ohjelmistoprojekti/domain/Vastaus.java
+++ src/main/java/salen/palikat/ohjelmistoprojekti/domain/Vastaus.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Vastaus {
@@ -11,5 +13,38 @@ public class Vastaus {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	private String vastaus;
+	@ManyToOne
+	@JoinColumn(name = "kysymys")
+	private Kysymys kysymys;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getVastaus() {
+		return vastaus;
+	}
+	public void setVastaus(String vastaus) {
+		this.vastaus = vastaus;
+	}
+	public Kysymys getKysymys() {
+		return kysymys;
+	}
+	public void setKysymys(Kysymys kysymys) {
+		this.kysymys = kysymys;
+	}
+	public Vastaus(String vastaus, Kysymys kysymys) {
+		super();
+		this.vastaus = vastaus;
+		this.kysymys = kysymys;
+	}
+	@Override
+	public String toString() {
+		return "Vastaus [id=" + id + ", vastaus=" + vastaus + ", kysymys=" + kysymys + "]";
+	}
+
 
 }
