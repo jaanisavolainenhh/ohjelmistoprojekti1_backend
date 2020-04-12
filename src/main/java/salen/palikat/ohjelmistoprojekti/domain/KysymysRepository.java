941387c1f5e4ef2922e4fb8a1a4a44155fb2f80a
jaani <bgh273@myy.haaga-helia.fi>
2020-04-12 15:31:24 +0300

    Crossoriginia että reacti toimis

--- src/main/java/salen/palikat/ohjelmistoprojekti/domain/KysymysRepository.java
+++ src/main/java/salen/palikat/ohjelmistoprojekti/domain/KysymysRepository.java
@@ -2,7 +2,9 @@
 package salen.palikat.ohjelmistoprojekti.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin //Lisätty jotta reactiappi  toimisi  https://docs.spring.io/spring-data/rest/docs/current/reference/html/#reference
 public interface KysymysRepository extends CrudRepository<Kysymys, Long> {
 	
 }
